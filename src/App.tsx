c90e01f32f4bf68bbdd27318f8fbea013f3b51be
Aleksey Pekhterev <qudaeo@yandex.ru>
2022-10-21 23:45:52 +0700

    add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path={'/'} element={<div>Hello world!</div>} />
+      <Route path={'/signup'} element={<div>sign up</div>} />
+      <Route path={'/login'} element={<div>login</div>} />
+      <Route path={'/products'} element={<div>products</div>} />
+      <Route path={'/*'} element={<div>404</div>} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
